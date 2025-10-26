7860b818769c51f2b5b72d2246d1bb22937829c7
aidenjcheng <aidenjcheng12@gmail.com>
2025-10-25 22:50:54 -0400

    Initial Commit

---
+++ hisadas/hisadasApp.swift
@@ -0,0 +1,17 @@
+//
+//  hisadasApp.swift
+//  hisadas
+//
+//  Created by Michelle on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct hisadasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
